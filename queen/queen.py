b2f0fe69d0730b08f8d5fb39c5941d9849d9d0d7
C272 <c272@protonmail.com>
2019-04-09 12:35:28 +0100

    Got initial connection working.

--- queen/queen.py
+++ queen/queen.py
@@ -1,16 +1,14 @@
 from microbit import *
 import radio
-import ujson
 
-class Command():
-    def __init__(self, function, *arg):
-        self.function = function
-        self.args = arg
-    
-
+#Enabling the display and radio.
 display.on()
 radio.on()
+
+#Configuring the radio for group 1.
 radio.config(group=1)
+
+#Constantly sending sum 3 4.
 while True:
-    radio.send("sum 2 9")
-    display.scroll("EPIC")
+    #Casting to check for clients.
+    radio.send("worker_request")
