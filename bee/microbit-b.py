1b3c110326cc6e64ba20d7f30f86d1d7d7ffffa4
Damian Heaton <dh64784@gmail.com>
2019-04-09 11:34:58 +0000

    String communication, basic functionality
    - Can now communicate with queen over space-delimited strings
    - Executes `sum a b` sent over BLE radio.

--- bee/microbit-b.py
+++ bee/microbit-b.py
@@ -1,11 +1,29 @@
+"""
+Worker accepts a space-delimited string sent over BLE radio.
+Format: function param1 param2 ...
+
+Response: "ERROR"|"SUCCESS" respVal
+"""
+
 from microbit import *
 import radio
 
+# define constants
+GROUP = 1
+
 radio.on()
-radio.config(group=1)
+radio.config(group=GROUP)
 display.on()
+
+def sum(a, b):
+    a = int(a)
+    b = int(b)
+    return a + b
+
 while True:
     recv = radio.receive()
-    if recv == "FOO":
-        display.on()
-        display.scroll("FOO")
+    if recv is not None:
+        msg = str(recv)
+        params = msg.split(" ")
+        if params[0] == "sum":
+            display.scroll(sum(params[1], params[2]))
