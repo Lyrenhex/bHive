6f1fc219afb697da86c84ac11dd615fb20ca09a0
C272 <c272@protonmail.com>
2019-04-09 13:35:11 +0100

    Added  client connections using UIDs.

--- queen/queen.py
+++ queen/queen.py
@@ -11,16 +11,27 @@ radio.on()
 #Configuring the radio for group 1.
 radio.config(group=1)
 
-def received(input):
-	id = str(input).split(" ")[0]
-	if id not in clients:
-		clients.append(id)
-	
+#Parsing received parameters.
+def parseReceived(input):
+    #Extracting parameters from message.
+    params = str(input).split(" ")
+
+    #Switching for different commands.
+    if params[0] == "pong":
+        #Adding ID, if not already in the list.
+        if params[1] not in clients:
+            clients.append(params[1])
 
 #Constantly sending worker_request.
 while True:
     #Casting to check for clients.
-    radio.send("worker_request")
-    receive = radio.receive()
-    if receive != None:
-    	received(receive)
+    if button_a.is_pressed():
+        radio.send("ping")
+
+    #Parsing any responses.
+    received = radio.receive()
+    if received != None:
+        parseReceived(receive)
+
+    #Showing current number of clients.
+    display.show(str(len(clients)))
