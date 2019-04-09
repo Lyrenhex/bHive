836cd7ebab565ee6a61ac353881add3e77664cb8
Damian Heaton <dh64784@gmail.com>
2019-04-09 16:29:25 +0000

    Better worker-queen synchronisation

--- bee/microbit-b.py
+++ bee/microbit-b.py
@@ -1,8 +1,9 @@
 """
 Worker accepts a space-delimited string sent over BLE radio.
-Format: function param1 param2 ...
+Format: macAddr function param1 param2 ...
 
-Response: "ERROR"|"SUCCESS" respVal
+Response: [ProcName] [Unique MicroBit ID] [...Responses] 
+          err [error code] [error message]
 """
 
 from microbit import *
@@ -11,32 +12,51 @@ import radio
 
 # define constants
 GROUP = 1
+RESERVED_FUNCS = [
+    "startProcess",
+    "endProcess",
+    "sendResponse",
+    "sendError"
+]
 
+# set up mac address-based unique id
 macAddr = machine.unique_id()
 macAddr = '{:02x}{:02x}{:02x}{:02x}'.format(macAddr[0], macAddr[1], macAddr[2], macAddr[3])
 
+# enable BLE and Display
 radio.on()
 radio.config(group=GROUP)
 display.on()
 
-def sum(a, b):
-    a = int(a)
-    b = int(b)
-    return a + b
+# visual marker for when Microbit is busy computing
+def startProcess():
+    display.show(Image.SQUARE_SMALL)
+def endProcess():
+    display.clear()
 
-while True:
-    recv = radio.receive()
-    if recv is not None:
-        msg = str(recv)
-        params = msg.split(" ")
-        if params[0] == "ping": # ping check -- we're available -- pong!
-            radio.send("pong " + macAddr)
-        elif params[1] == "sum":
-            if params[0] == macAddr:
-                display.scroll(sum(params[2], params[3]))
+# Function to send computed function response to Queen, using comms schema
+def sendResponse(procName, *responses):
+    resp = procName + " " + macAddr
+    for val in responses:
+        resp += " " + str(val)
+    radio.send(resp) 
+def sendError(errNum, errMessage):
+    radio.send("err " + str(errNum) + " " + str(errMessage))
 
-primeNumList = [2,3,5]
-testNum = 3
+# Exemplar computation function - sum a and b
+def sum(*args):
+    if len(args) > 1:
+        total = 0
+        try:
+            for i in args:
+                total += int(i)
+        except TypeError:
+            sendError(1, "Non-integer parameter supplied.")
+            return None
+        return total
+    else:
+        sendError(1, "Insufficient number of parameters.")
+        return None
 
 def findPrime(TestNum):
     prime = 0
@@ -52,6 +72,31 @@ def findPrime(TestNum):
 
 def addPrime(newPrime):
     primeNumList.append(newPrime)
+
+# Main loop
+while True:
+    # keep polling BLE for data
+    recv = radio.receive()
+    # if not empty
+    if recv is not None:
+        # parse the radio data into something useful
+        msg = str(recv)
+        params = msg.split(" ")
+        
+        # note that we're gonna be computing now
+        startProcess()
+        if params[0] == "ping": # ping check -- we're available -- pong!
+            radio.send("pong " + macAddr)
+        # check that the instruction is intended for us
+        elif params[0] == macAddr:
+            if (params[1] in locals()) and (params[1] not in RESERVED_FUNCS):
+                response = locals()[params[1]](params[2:])
+                if response is not None:
+                    sendResponse(params[1], response)
+        endProcess()
+
+primeNumList = [2,3,5]
+testNum = 3
 
 primeTrue = findPrime(testNum)
 addPrime(testNum)
