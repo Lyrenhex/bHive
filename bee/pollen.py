5ea291fb5fc148811ca786304107421ddc21e50e
C272 <c272@protonmail.com>
2019-04-10 21:01:53 +0100

    Add full working lisp-like pollen script language.

--- bee/pollen.py
+++ bee/pollen.py
@@ -1,139 +1,161 @@
 ########################
 ## POLLEN INTERPRETER ##
 ########################
+import math as maths
+import operator as ops
 
-# GLOBAL POLLEN PROPERTIES
-# Characters which are to be classed as individual tokens.
-iTokenCharacters = ["+", "-", "*", "/", "(", ")", "[", "]", ";", ",", "{", "}", " "]
-# Characters which are to be skipped when parsing.
-skipCharacters = []
-# Tokens which are used as a command.
-commandTokens = ["return", "func"]
-# User-defined functions.
-functions = []
+### POLLEN ENVIRONMENT VARIABLES ##
 
-# Action types:
-# 0: Add/take
-# 1: Multiply/divide
-# 2: Function
-# 3: Comparison
+# Gets back the standard global environment.
+def getStandardEnvironment():
+    env = {}
+    env.update(vars(maths))
+    default_env = {
+        '+': ops.add,
+        '-': ops.sub,
+        '*': ops.mul,
+        '/': ops.truediv,
+        '<': ops.lt,
+        '>': ops.gt,
+        '<=': ops.le,
+        '>=': ops.ge,
+        '=': ops.eq,
+        'abs': abs,
+        'append': ops.add,
+        'max': max,
+        'min': min,
+        'not': ops.not_,
+        'round': round
+    }
+    env.update(default_env)
 
-# State types:
-# default: The state the interpreter starts in. No special action.
-# funcDefine: Defining a function.
-# - funcDefineParams: Defining params in a function.
+    return env
 
+# Initializes the current Pollen standard library.
+def initStdLib():
+    parsePollen(
+        """
+        """
+    )
 
-class Action:
-    def __init__(self, actionType, actionValue):
-        self.actionType = actionType
-        self.actionValue = actionValue
+# Resets the current environment.
+env = getStandardEnvironment()
+def resetEnv():
+    env = getStandardEnvironment()
 
-class Function:
-    def __init__(self, *funcParams, funcStartPos, funcEndPos, name):
-        self.funcParams = funcParams
-        self.funcStartPos = funcStartPos
-        self.funcEndPos = funcEndPos
-        self.name = name
+def getLocalEnv(localDict, globalDict):
+    returnEnv = {}
+    # For each item, take the global from duplicated vars.
+    for item, value in globalDict.items():
+        if item not in localDict:
+            returnEnv[item] = value
+    returnEnv.update(localDict)
+    return returnEnv
+
+# A lambda procedure call.
+class Procedure(object):
+    def __init__(self, params, body, env):
+        self.params = params
+        self.body = body
+        self.env = params, body, env
+    def __call__(self, *args):
+        # Need to separate global namespace from local namespace.
+        local_env =  getLocalEnv(dict(zip(self.params, args)), env)
+        return evaluate(self.body, local_env)
+
+# Evaluates the set of instructions parsed by tokenparse.
+Number = (int, float)
+def evaluate(instr, currentEnv):
+    # Referencing a variable.
+    if isinstance(instr, str):
+        return currentEnv[instr]
+    # Referencing a constant.
+    elif isinstance(instr, Number):
+        return instr
+    # A conditional statement.
+    elif instr[0]=="if":
+        # Check against a tuple, which is made up of the current list.
+        (_, test, conseq, alt) = instr
+        exp = (conseq if evaluate(test, currentEnv) else alt)
+        return evaluate(exp, currentEnv)
+    # Defines a variable on the current environment.
+    elif instr[0]=="def":
+        (_, var, exp) = instr
+        currentEnv[var] = evaluate(exp, currentEnv)
+        print(currentEnv)
+    # Defining or executing a lambda.
+    elif instr[0] == 'lambda':
+        (_, params, body) = instr
+        return Procedure(params, body, currentEnv)
+    else:
+        # Existing process that needs to be run. Get process and arguments, then return.
+        proc = evaluate(instr[0], currentEnv)
+        args = [evaluate(arg, currentEnv) for arg in instr[1:]]
+        return proc(*args)
 
 # Tokenizes a given script, returns.
 def tokenize(script):
-    # Split the string up into tokens.
-    tokens = []
-    token = ""
-    for char in script:
-        # Check against token characters here. If individual token, add and reset.
-        if char in iTokenCharacters:
-            if token != "" and token != " ":
-                tokens.append(token)
-            if char != " ":
-                tokens.append(char)
-            token = ""
-        elif char not in skipCharacters:
-            token += char
-    return tokens
+    # Use a char hack to split the string into bracket operations.
+    return script.replace('(', ' ( ').replace(')', ' ) ').split()
 
-# Defines a function.
-def defineFunc(tokens, startIndex):
-    params = []
+# "Atomizes" the given token, to convert from all symbols to numbers and symbols.
+def atomize(token):
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return token
 
-    #Finding the start of a bracket, adding params.
-    startIndexDelta = -1
-    inParams = False
-    for token in tokens:
-        if token=="(":
-            #Start params.
-            inParams = True
-        if token==")":
-            startIndexDelta = tokens.index(token) + 2
-            #End params.
-            break
-        if inParams==True:
-            params.append(token)
+# Parses the given set of tokens into instructions.
+def tokenparse(tokens):
+    # Checking no EOF.
+    if len(tokens) == 0:
+        return "p_err 0"
     
-    #Checking start delta has ben found.
-    if startIndexDelta==-1:
+    # Getting token.
+    token = tokens.pop(0)
+
+    # Checking for open bracket.
+    if token=="(":
+        child = []
+        # Only go until end bracket.
+        while tokens[0] != ")":
+            child.append(tokenparse(tokens))
+        # Pop off the end ")".
+        tokens.pop(0)
+        return child
+    elif token==")":
         return "p_err 1"
-
-    #Finding the end of the func.
-    startIndex += startIndexDelta
-    endIndex = -1
-    for token in tokens:
-        if token=="}":
-            endIndex = startIndex + tokens.index(token)
-    
-    #Checking end has been found.
-    if endIndex == -1:
-        return "p_err 2"
-
-    #Returning func.
-    return Function(params, startIndex, endIndex, tokens[0])
+    else:
+        return atomize(token)
 
 
-# Lexes a set of tokens into actions.
-def lex(tokens):
-    actionList = []
-    state = "default"
-
-    for token in tokens:
-        #Default state checks.
-        if state=="default":
-            if token in iTokenCharacters and token!=";":
-                #Calculation where there shouldn't be.
-                return "p_err 0"
-            elif (token=="func"):
-                state = "funcDefine"
-        #Currently defining a function.
-        if state=="funcDefine":
-            #Define elsewhere.
-            index = tokens.index(token)
-            func = defineFunc(token[index:], index)
-
-            #Errored in func call.
-            if type(func) is str:
-                return func
-            else:
-                functions.append(func)
-
-    print("bleh")
-    #Return the list.
-    return actionList
-
-# Parses a given pollen script as a string.
-def parsePollen(script):
-    #Tokenizing.
+def parsePollenLine(script):
+    # Tokenizing the pollen script.
     tokens = tokenize(script)
-    print(tokens)
+    # Parsing the tokens.
+    parsed = tokenparse(tokens)
 
-    #Lexing into action list.
-    actionList = lex(tokens)
-    # If string returned, error in lexing.
-    if type(actionList) == str:
-        return actionList
+    # Evaluating the tokens.
+    return evaluate(parsed, env)
 
+def parsePollen(script):
+    # Splitting the lines by ";".
+    lines = script.split(";")
 
-print(parsePollen(
-    "func b() { }"
-))
+    # Executing all lines apart from last.
+    nonReturnLines = lines[:-1]
+    for line in nonReturnLines:
+        parsePollenLine(line)
+    
+    # Returning last.
+    return parsePollenLine(lines[-1])
+
+print(parsePollen("""
+(def gamer (lambda (x) (+ x 4)));
+(gamer 6)
+"""))
 
 #### END POLLEN INTERPRETER ####
--- docs/errors.md
+++ docs/errors.md
@@ -4,6 +4,5 @@
 2: Invalid function
 
 # Pollen Codes (POLLEN::ERROR)
-0: Invalid expression outside of parse.
-1: No function start detected.
-2: No function end detected.
+0: Unexpected End of File
+1: Unexpected end of bracket ")"
